01bb623cd72d2d6f90e1648228005e6fd57e297c
Carter Koehler <carterleekoehler@gmail.com>
2018-02-24 13:29:02 -0600

    Adding activity for Login

--- app/src/main/java/edu/lonestar/droplet/LoginActivity.java
+++ app/src/main/java/edu/lonestar/droplet/LoginActivity.java
@@ -6,15 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
-
 }
+
