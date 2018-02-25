641c2d3796afeae1122aab0a9efa66bfcfb0f004
Trenton Telge <darkforce100@users.noreply.github.com>
2018-02-25 02:03:08 -0600

    Switched control panel layouts for redesign

--- app/src/main/java/edu/lonestar/droplet/LenderControlPanelActivity.java
+++ app/src/main/java/edu/lonestar/droplet/LenderControlPanelActivity.java
@@ -1,11 +1,7 @@
 package edu.lonestar.droplet;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 public class LenderControlPanelActivity extends AppCompatActivity {
