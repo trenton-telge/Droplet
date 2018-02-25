090553732a196fc64c7cbee703476aa33e534bf9
Trenton Telge <darkforce100@users.noreply.github.com>
2018-02-25 01:35:08 -0600

    Prototype for lender control panel

--- app/src/main/java/edu/lonestar/droplet/LenderControlPanelActivity.java
+++ app/src/main/java/edu/lonestar/droplet/LenderControlPanelActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class LenderControlPanelActivity extends AppCompatActivity {
 
@@ -13,17 +14,8 @@ public class LenderControlPanelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lender_control_panel);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView name = findViewById(R.id.nameplate);
+        name.setText(String.format("%s %s", MainActivity.loggedUser.firstName, MainActivity.loggedUser.lastName));
     }
 
 }
