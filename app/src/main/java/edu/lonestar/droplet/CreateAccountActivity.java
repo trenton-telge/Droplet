f5e6f7e0dfe3e9050700b01c4087c4938d69306c
Trenton Telge <darkforce100@users.noreply.github.com>
2018-02-24 19:35:46 -0600

    Added loader activity

--- app/src/main/java/edu/lonestar/droplet/CreateAccountActivity.java
+++ app/src/main/java/edu/lonestar/droplet/CreateAccountActivity.java
@@ -2,6 +2,8 @@ package edu.lonestar.droplet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        Button createAccountButton = findViewById(R.id.createAccountFinalizeButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/lonestar/droplet/LoaderActivity.java
@@ -0,0 +1,13 @@
+package edu.lonestar.droplet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+    }
+}
